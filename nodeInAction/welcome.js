3f3010af7898652fbe216af66949c756f8b95160
sahil_rao <sahil.rao@cimpress.com>
2021-05-13 03:41:41 +0530

    First commit

---
+++ nodeInAction/welcome.js
@@ -0,0 +1,7 @@
+// This is basic hello world node Program
+//Node works same as JS hence only one file is required for this example
+//Since node can run outside the browser, we use a different command to see the output in the terminal
+// In terminal, run : node welcome.js
+//Runnig this will run js outside the browser, hence Node in action
+
+console.log("Hello world")
